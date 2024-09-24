75cfcb62a45d88f31f674d7239f82e6553ddad1b
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-09-24 05:54:07 -0500

    added backwards-compatibility fix for panels

--- moredoor.lua
+++ moredoor.lua
@@ -17,7 +17,7 @@ nodecore.register_door("nc_optics", "glass_opaque", "Chromatic", "nc_lode:rod_te
 nodecore.register_door("nc_lode", "block_annealed", "Lode", "nc_lode:rod_tempered", 5)
 
 -- ================================================================== --
--- <> One Does Not Simply Walk Into Moredoor <> -- Never gets old --
+-- <> One Does Not Simply Walk Into Moredoor <> -- 
 -- ================================================================== --
 
 -- I see what the problem with adding allfaces and allfaces_optional doortypes is now.
@@ -32,4 +32,18 @@ nodecore.register_door("nc_lode", "block_annealed", "Lode", "nc_lode:rod_tempere
 --minetest.override_item(modname.. ":panel_glass_float",{drawtype = "normal", use_texture_alpha = true,})
 --minetest.override_item(modname.. ":door_glass_float",{drawtype = "normal", use_texture_alpha = true,})
 
+-- ================================================================== --
+-- <> One Does Not Simply Walk Into Moredoor <> -- Never gets old --
+-- ================================================================== --
 
+minetest.register_alias(modname.. ":panel_cornplank",				"nc_doors:panel_cornplank")
+minetest.register_alias(modname.. ":door_cornplank",				"nc_doors:door_cornplank")
+
+minetest.register_alias(modname.. ":panel_thatch",				"nc_doors:panel_thatch")
+minetest.register_alias(modname.. ":door_thatch",				"nc_doors:door_thatch")
+
+minetest.register_alias(modname.. ":panel_glass_opaque",				"nc_doors:panel_glass_opaque")
+minetest.register_alias(modname.. ":door_glass_opaque",				"nc_doors:door_glass_opaque")
+
+minetest.register_alias(modname.. ":panel_block_annealed",				"nc_doors:panel_block_annealed")
+minetest.register_alias(modname.. ":door_block_annealed",				"nc_doors:door_block_annealed")
