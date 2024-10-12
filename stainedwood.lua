fe08e0e26d4eb6802288c0bb64a2f9d50cf0114b
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-12 00:52:47 -0500

    darker stained wood variants
    you can now add more resin to stained wood to darken it further, though this is only an aesthetic change.
    it is also now possible to scrape the resin layer off of the plank to get regular planks back to use for crafting recipes.

--- stainedwood.lua
+++ stainedwood.lua
@@ -1,12 +1,8 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore
-    = minetest, nodecore
+local minetest, nodecore, math
+    = minetest, nodecore, math
 -- LUALOCALS > ---------------------------------------------------------
-
 local modname = minetest.get_current_modname()
-
-local plank = modname .. ":plank_stained"
-
 local rfcall = function(pos, data)
 	local ref = minetest.get_player_by_name(data.pname)
 	local wield = ref:get_wielded_item()
@@ -16,11 +12,16 @@ end
 
 local wood = "nc_woodwork_plank.png"
 local resin = modname.. "_sap.png"
-local stain = "(" ..resin.. "^[opacity:140)^[colorize:black:64"
 
-minetest.register_node(plank, {
+for i = 1,6 do
+ local stup = i+1
+ local black = i*32
+ local plank = modname .. ":plank_stained_" ..i
+ local stain = "(" ..resin.. "^[opacity:140)^[colorize:black:" ..black
+
+	minetest.register_node(plank, {
 		description = "Stained Wooden Plank",
---		tiles = {"nc_woodwork_plank.png^[colorize:#704214:100"}, --Sepia
+	--	tiles = {"nc_woodwork_plank.png^[colorize:#704214:100"}, --Sepia
 		tiles = {wood.. "^(" ..stain.. ")"},
 		groups = {
 			choppy = 1,
@@ -31,8 +32,8 @@ minetest.register_node(plank, {
 		sounds = nodecore.sounds("nc_tree_woody")
 	})
 
-local function bashrecipe(thumpy, normaly)
-	nodecore.register_craft({
+	local function bashrecipe(thumpy, normaly)
+		nodecore.register_craft({
 			label = "bash stained planks to sticks",
 			action = "pummel",
 			toolgroups = {thumpy = thumpy},
@@ -45,18 +46,40 @@ local function bashrecipe(thumpy, normaly)
 				{name = "nc_tree:stick 2", count = 4, scatter = 5}
 			}
 		})
-end
-bashrecipe(3, 1)
-bashrecipe(5, -1)
+	end
+	bashrecipe(3, 1)
+	bashrecipe(5, -1)
 
-nodecore.register_craft({
+	nodecore.register_craft({
 		label = "stain wood planks",
 		action = "pummel",
 		wield = {name = modname.. ":lump_sap"},
 		after = rfcall,
 		nodes = {
-				{match = "nc_woodwork:plank", replace = modname .. ":plank"}
+				{match = "nc_woodwork:plank", replace = modname.. ":plank_stained_1"}
 			}
 	})
 
-minetest.register_alias(modname .. ":plank",					modname .. ":plank_stained")
+	nodecore.register_craft({
+		label = "stain wood planks",
+		action = "pummel",
+		wield = {name = modname.. ":lump_sap"},
+		after = rfcall,
+		nodes = {
+				{match = modname.. ":plank_stained_" ..i, replace = modname.. ":plank_stained_" ..stup}
+			}
+	})
+	
+	nodecore.register_craft({
+		label = "scrape stain from planks",
+		action = "pummel",
+		duration = stup,
+		toolgroups = {choppy = 4},
+		indexkeys = {plank},
+		nodes = {
+			{match = plank, replace = "nc_woodwork:plank"}
+		}
+	})
+end
+minetest.register_alias(modname .. ":plank",				modname .. ":plank_stained_1")
+minetest.register_alias(modname.. ":plank_stained",			modname.. ":plank_stained_1")
